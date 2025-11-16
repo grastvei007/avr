c8d03132386d8da8a8bd9999a386a358a3439bc3
Stein Gunnar Grastveit <grastvei@pvv.org>
2025-11-16 09:53:44 +0100

    Update usart communication
    Transmit with interrupt. Use buffer for Rx and Tx

--- usart.cpp
+++ usart.cpp
@@ -1,4 +1,4 @@
-/*This file is part of June.
+/* This file is part of June.
 
 June is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -14,93 +14,136 @@ You should have received a copy of the GNU General Public License
 along with Foobar.  If not, see <https://www.gnu.org/licenses/>.*/
 
 #include "usart.h"
-#include "circularbuffer.h"
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/setbaud.h>
+#include <string.h>
 
-#define ASYNCHRONOUS (0<<UMSEL00) // USART Mode Selection
-
-#define DISABLED    (0<<UPM00)
-#define EVEN_PARITY (2<<UPM00)
-#define ODD_PARITY  (3<<UPM00)
-#define PARITY_MODE  DISABLED // USART Parity Bit Selection
-
-#define ONE_BIT (0<<USBS0)
-#define TWO_BIT (1<<USBS0)
-#define STOP_BIT ONE_BIT      // USART Stop Bit Selection
-
-#define FIVE_BIT  (0<<UCSZ00)
-#define SIX_BIT   (1<<UCSZ00)
-#define SEVEN_BIT (2<<UCSZ00)
-#define EIGHT_BIT (3<<UCSZ00)
-#define DATA_BIT   EIGHT_BIT  // USART Data Bit Selection
-
-#define RX_COMPLETE_INTERRUPT         (1<<RXCIE0)
-#define DATA_REGISTER_EMPTY_INTERRUPT (1<<UDRIE0)
-
-volatile uint8_t USART_send_value;
-CircularBuffer transmitBuffer = buffer_create();
+USART_BUFFER usart_rx_buffer = {{0}, 0, 0};
+USART_BUFFER usart_tx_buffer = {{0}, 0, 0};
 
 
-ISR(USART_UDRE_vect)
+void usart_transmit(uint8_t data)
 {
-    UDR0 = USART_send_value;
-    UCSR0B &= ~DATA_REGISTER_EMPTY_INTERRUPT; // Disables the Interrupt, uncomment for one time transmission of data
+    unsigned int next_head = (usart_tx_buffer.head + 1) % USART_BUFFER_SIZE;
+
+    //wait
+//    while(next_head == usart_tx_buffer.tail);
+    
+    usart_tx_buffer.buffer[usart_tx_buffer.head] = data;
+    usart_tx_buffer.head = next_head;
+
+    // enable TX interupt
+    USART0_CONTROL |= (1 << UDRIE0);
+
+//    while(!(UCSR0A & (1 << UDRE0)));
+//    UDR0 = data;
 }
 
-void USART_send(char data)
+void usart_transmit_bytes(char bytes[], unsigned int size)
 {
-    while(!(UCSR0A & (1<<UDRE0)));
-        UDR0 = data;
-}
-
-void USART_putstring(char* StringPtr)
-{
-    while(*StringPtr != 0x00){
-         USART_send(*StringPtr);
-        StringPtr++;
-    }
-}
-
-void USART_putMessage(char *aMsg, int size)
-{
-    for(int i=0; i<size; ++i)
+    for(unsigned int i=0; i < size; ++i)
     {
-        USART_send(aMsg[i]);
+        usart_transmit(bytes[i]);
     }
 }
 
+void usart_transmit_string(char *data)
+{
+/*    while(data)
+    {
+        usart_transmit(*(++data));
+    }*/
 
-void USART_init()
+  unsigned char c = *data;
+  while(c)
+  {
+ //   while(!(UCSR0A & (1 << UDRE0)));
+   // UDR0 = c;
+    usart_transmit(c);
+    c = *(++data);
+  }
+}
+
+void usart_init(unsigned int baud)
 {
     UBRR0H = (uint8_t)(BAUD_PRESCALLER>>8);
     UBRR0L = (uint8_t)(BAUD_PRESCALLER);
+//    UDBR0 = USART_BAUDRATE(baud);
 
-   //  UCSR0C |= (1<<UCSZ01)|(1<<UCSZ00);
-    UCSR0C = ASYNCHRONOUS | PARITY_MODE | STOP_BIT | DATA_BIT;
-    //enable reception and RC complete interrupt
-     UCSR0B |= (1<<TXEN0) | (1<<RXEN0)|(1<<RXCIE0);
+    // enable rx and tx
+//    USART0_CONTROL |= (1 << RXEN0) | (1 << TXEN0);
+
+    // enable rx interupt
+//    USART0_CONTROL |= (1 << RXCIE0);
+
+    // Set frame format: 8data, 1 stop bit
+//    USART0_STATUS = (1 << UCSZ01) | (1 << UCSZ00);
+
+    // enable transmit and receive
+    UCSR0B |= (1 << TXEN0) | (1 << RXCIE0);
+
+    UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
+
+    UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);
      
+    // enable global interupt
     sei();
 }
 
-
-void USART_buffer_append(char *msg, unsigned int size)
+void usart_put_char_in_buffer(unsigned char c)
 {
-    for(unsigned int i=0; i<size; ++i)
+    unsigned int i = (unsigned int)(usart_rx_buffer.head + 1) & USART_BUFFER_SIZE;
+
+    if(i != usart_rx_buffer.tail)
     {
-        buffer_write(&transmitBuffer, msg[i]);
+        usart_rx_buffer.buffer[usart_rx_buffer.head] = c;
+        usart_rx_buffer.head = i;
     }
 }
 
-
-void USART_buffer_send()
+void usart_clear_buffer()
 {
-    if(!buffer_empty(&transmitBuffer))
+  usart_rx_buffer.head = 0;
+  usart_rx_buffer.tail = 0;
+  usart_tx_buffer.head = 0;
+  usart_tx_buffer.tail = 0;
+}
+
+uint8_t usart_data_available()
+{
+    return (uint8_t)(USART_BUFFER_SIZE + usart_rx_buffer.head - usart_rx_buffer.tail) % USART_BUFFER_SIZE;
+}
+
+uint8_t usart_read_data()
+{
+    if(usart_rx_buffer.head == usart_rx_buffer.tail)
+        return -1;
+    else
     {
-        USART_send_value = buffer_read(&transmitBuffer);
-        UCSR0B |= DATA_REGISTER_EMPTY_INTERRUPT;
+        uint8_t data = usart_rx_buffer.buffer[usart_rx_buffer.tail];
+        usart_rx_buffer.tail = (unsigned int)(usart_rx_buffer.tail + 1) % USART_BUFFER_SIZE;
+        return data;
+    }
+}
+
+ISR(USART_RX_vect)
+{
+    unsigned char data = UDR0;
+    usart_put_char_in_buffer(data);
+}
+
+ISR(USART_UDRE_vect)
+{
+    if(usart_tx_buffer.head != usart_tx_buffer.tail)
+    {
+        UDR0 = usart_tx_buffer.buffer[usart_tx_buffer.tail];
+        usart_tx_buffer.tail = (usart_tx_buffer.tail + 1) % USART_BUFFER_SIZE;
+    }
+    else
+    {
+        USART0_CONTROL &= ~(1 << UDRIE0);
     }
 }
 
--- usart.h
+++ usart.h
@@ -16,21 +16,42 @@ along with Foobar.  If not, see <https://www.gnu.org/licenses/>.*/
 #ifndef USART_H
 #define USART_H
 
-#define F_CPU 12000000UL
-#define BAUDRATE 9600
-#define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 12UL))) - 1)
+// Allow F_CPU and BAUD to be set from Makefile,
+// but use some defaults if not.
+#ifndef F_CPU
+  #define F_CPU 12000000UL
+#endif
+#ifndef BAUD
+  #define BAUD 9600
+#endif
+#define BAUD_PRESCALLER (((F_CPU / (BAUD * 12UL))) - 1)
+
+#define USART0_CONTROL UCSR0B
+#define USART0_STATUS UCSR0C
+
+typedef unsigned char uint8_t;
+
+#define USART_BUFFER_SIZE 128
+
+typedef volatile struct
+{
+  unsigned char buffer[USART_BUFFER_SIZE];
+  unsigned int head;
+  unsigned int tail;
+}USART_BUFFER;
+
+extern USART_BUFFER usart_rx_buffer;
+extern USART_BUFFER usart_tx_buffer;
 
 
+void usart_init(unsigned int baud);
+void usart_transmit(uint8_t data);
+void usart_transmit_bytes(char data[], unsigned int size);
+void usart_transmit_string(char *data);
 
+void usart_put_char_in_buffer(unsigned char c);
+void usart_clear_buffer();
+uint8_t usart_data_available();
 
-void USART_init(void);
-// UnBuffered sending
-void USART_send(char data);
-void USART_putstring(char* StringPtr);
-void USART_putMessage(char *aMsg, int size);
-
-//Buffered sending
-void USART_buffer_append(char *msg, unsigned int size); ///< append string to buffer
-void USART_buffer_send(); ///< send first char from buffer
-
+uint8_t usart_read_data();
 #endif
